2f0bbef67ebe6c446a754216689f0cb335abcee1
jarin <rafajarin2022@gmail.com>
2023-06-05 01:02:28 +0600

    this is my first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/*!
+ * Template Name/Project Name: XYZ
+ * Developer Name: Mr ABC
+ * Conatct: abc@gmail.com
+ * Visit: https://abc.info
+ */
+ /*common class start*/
+ /*for custom fonts*/
+  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&family=Source+Sans+Pro&display=swap');
+ 
